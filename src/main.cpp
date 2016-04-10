5a8fa8045f8e61ba40e0e16e6c1506f077546a0a
Joseph Cutler <joseph_cutler@watkinson.org>
2016-04-10 16:26:05 -0400

    steps towards a raytracer

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,13 @@
 #include <array>
 #include <cstdio>
 #include <vector>
+#include <limits>
 #include "Math/Vec2/Vec2.h"
 #include "Math/Vec3/Vec3.h"
 #include "Math/Vec4/Vec4.h"
 #include "Math/Quat/Quat.h"
 #include "Math/Mat4/Mat4.h"
-#include "ObjFile/ObjFile.h"
+#include "Math/Ray/Ray.h"
 #include "Math/Tree/Tree.h"
 
 #include "Graphics/Graphics.h"
@@ -17,121 +18,93 @@
 #include <GLUT/glut.h>
 //https://github.com/ssloy/tinyrenderer/wiki/Lesson-1:-Bresenham%E2%80%99s-Line-Drawing-Algorithm
 
-#define DEG(x) (x*M_PI/180)
 
+typedef struct Sphere{
+	Vec3 pos;
+	float r;
+} Sphere;
+
+/**
+ * @note: x0<=x1
+ */
+bool quad_solve(const float a, const float b, const float c, float &x0, float &x1){
+	auto discr = SQ(b) - 4*a*c;
+	if(discr < 0) return false;
+	x0 = (-b + sqrt(discr))/(2*a);
+	x1 = (-b - sqrt(discr))/(2*a);
+	if(x1 < x0) std::swap(x1,x0);
+	return true;
+}
+
+//https://en.wikipedia.org/wiki/Line%E2%80%93sphere_intersection
+bool sphere_ray_intersect_dist(const Sphere &sphere, const Ray &ray, float &d0, float &d1){
+	auto a = SQ(ray.dir.dot(ray.dir));
+	auto b = 2*(ray.dir.dot(ray.orig - sphere.pos));
+	auto c = SQ((ray.orig - sphere.pos).dot(ray.orig - sphere.pos)) - SQ(sphere.r);
+	d0 = std::numeric_limits<float>::infinity();
+	d1 = std::numeric_limits<float>::infinity();
+	auto hit = quad_solve(a,b,c,d0,d1);
+	return hit;
+}
 
 int main(int argc, char** argv){
 	(void) argc;
 	(void) argv;
 
-//	AVLTree<unsigned int> tree = AVLTree<unsigned int>(7);
-	AVLTree<int> tree = AVLTree<int>(8);
-	tree.Insert(3);
-	tree.Insert(10);
-	tree.Insert(1);
-	tree.Insert(6);
-	tree.Insert(4);
-	tree.Insert(7);
-	tree.Insert(14);
-	tree.Insert(13);
-
-//	tree.root->MakeLeft(2);
-//	tree.root->MakeRight(30);
-
-	tree.Print();
-
-//	tree.Apply([](unsigned int i){
-//			printf("%d\n",i);
-//			return i;
-//	});
-
-	auto tbl = Vertex(Vec4(-1,1,1),		{1,3,4});
-	auto tbr = Vertex(Vec4(1,1,1),		{0,2,5});
-	auto tfr = Vertex(Vec4(1,1,-1),		{1,3,6});
-	auto tfl = Vertex(Vec4(-1,1,-1),		{0,2,7});
-
-
-	auto bbl = Vertex(Vec4(-1,-1,1),	{0,7,5});
-	auto bbr = Vertex(Vec4(1,-1,1),		{1,4,6});
-	auto bfr = Vertex(Vec4(1,-1,-1),		{2,5,7});
-	auto bfl = Vertex(Vec4(-1,-1,-1),	{3,4,6});
-
-	std::vector<Vertex> cube;
-	cube.push_back(tbl);
-	cube.push_back(tbr);
-	cube.push_back(tfr);
-	cube.push_back(tfl);
-
-	cube.push_back(bbl);
-	cube.push_back(bbr);
-	cube.push_back(bfr);
-	cube.push_back(bfl);
-
-	//ObjFile diamond("diamond.obj");
-
-	auto t0 = Vec4(-100,-50,1);
-	auto t1 = Vec4(100,-50,5);
-	auto t2 = Vec4(0,100,1);
-
-	auto a0 = Vec4(-100,-50,2);
-	auto a1 = Vec4(100,-50,2);
-	auto a2 = Vec4(0,100,2);
-
-	std::array<Vec4,3> triangle = {{t0,t1,t2}};
-	std::array<Vec4,3> triangle2 = {{a0,a1,a2}};
-
-	(void) triangle;
-	(void) triangle2;
 
 	Graphics g = Graphics(1920/2,1080/2,"Window");
+	g.Clear();
 
-	float i = 0.05;
-	Quat q;
+	Sphere s0;
+	s0.pos = Vec3(0,0,100);
+	s0.r = 40.0;
 
-	SDL_Event e;
+	Vec3 camera_pos = Vec3(0,0,-100);
+	Vec3 screen_pos = Vec3(0,0,0);
+	Vec3 screen_top_left = screen_pos + Vec3(-g.width/2,g.height/2,0);
 
-	auto initial_rot = Quat::rotation((Vec3::J + Vec3::K).normalized(),DEG(90));
-	auto initial_trans = Mat4::Translation(0,0,100);
-	auto initial_scale = Mat4::Scale(100);
-	for(auto& v : cube){
-		v.pos = initial_trans * initial_scale * v.pos.rotate(initial_rot);
+	float a0 = 0;
+	float a1 = 0;
+	quad_solve(2,-1,-6,a0,a1);
+	printf("Solved quadratic. Roots: %.2f, %.2f\n",a0,a1);
+
+	for(int y = 0; y < g.height; ++y){
+		g.Clear();
+		for(int x = 0; x < g.width; ++x){
+			Ray cast_ray;
+			cast_ray.orig = camera_pos;
+
+			auto px_vec = screen_top_left - camera_pos;
+			px_vec.x += x;
+			px_vec.y -= y;
+			cast_ray.dir = px_vec;
+
+			float d0;
+			float d1;
+
+			auto hit = sphere_ray_intersect_dist(s0,cast_ray,d0,d1);
+			float dist = std::min(d0,d1);
+			(void) dist;
+
+			if(hit){
+				g.PutPixel(x,y,Color::Red);
+			}
+		}
+		g.Update();
 	}
 
-	//TODO:TRUE TO RUN
-	bool running =  false;
+	g.Update();
 
+	SDL_Event e;
+	bool running =  true;
 	while(running){
 		//TODO: DEBUGGING
-//		running = false;
 		if(SDL_PollEvent(&e)){
 			if(e.type == SDL_QUIT || e.window.event == SDL_WINDOWEVENT_CLOSE){
 				running = false;
 			}
 		}
-
-		i += 0.5;
-		const auto proj = Mat4::Projection();
-
-		g.Clear();
-
-
-//		g.Polygon(cube,Color::Green,[&](Vec4 v){
-//			auto scale = v.z;
-//			v = (proj * v) / scale;
-//			return v;
-//		});
-
-	//	g.Polygon(cube,Color::Green,Quat::rotation(Vec3::I,DEG(i)));
-
-//		auto rotation_axis = (Vec3::J).normalized();
-//		q = Quat::rotation(rotation_axis,DEG(i));
-//		for(auto& v : cube){
-//			v.pos = v.pos.rotate(q);
-//		}
-//		g.Polygon(cube,Color::Green);
-
-		g.Update();
-
+		
 	}
 
 	SDL_Quit();
