36ea466148db8cff6bd238887f491154ae3ad10c
Joseph Cutler <joseph_cutler@watkinson.org>
2016-04-10 16:19:49 -0400

    finished vec4

--- src/Math/Vec4/Vec4.cpp
+++ src/Math/Vec4/Vec4.cpp
@@ -1,3 +1,4 @@
+#include "macros.h"
 #include "Vec4.h"
 #include "Vec3/Vec3.h"
 #include "Vec3/Vec3.h"
@@ -13,9 +14,24 @@ const Vec4 Vec4::J = Vec4(0,1,0,1);
 const Vec4 Vec4::K = Vec4(0,0,1,1);
 const Vec4 Vec4::L = Vec4(0,0,0,1);
 
+float Vec4::mag() const{
+	return sqrt(SQ(x) + SQ(y) + SQ(z));
+}
 
-#define SQ(x) (x*x)
-#undef SQ
+Vec4 Vec4::operator+(const Vec4& v) const{
+	return Vec4(x+v.x,
+				y+v.y,
+				z+v.z,
+				w+v.w);
+}
+
+Vec4 Vec4::operator-(const Vec4& v) const{
+	return Vec4(x-v.x,
+				y-v.y,
+				z-v.z,
+				w-v.w);
+}
+
 
 Vec4 Vec4::operator*(float f) const{
 	return Vec4(x*f,y*f,z*f,w*f);
@@ -32,11 +48,30 @@ Vec4::operator Vec3() const{
 	return v;
 }
 
+Vec4 Vec4::normalized() const{
+	return *this/mag();
+}
+
+void Vec4::normalize(Vec4& v){
+	v = v.normalized();
+}
+
+float Vec4::dot(const Vec4& v) const{
+	return v.x*x + v.y*y + v.z*z + v.w*w;
+}
+
+float Vec4::dot(const Vec4& a, const Vec4& b){
+	return a.x*b.x + a.y*b.y + a.z*b.z + a.w*b.w;
+}
+
+Vec4 Vec4::cross(const Vec4& v) const{
+	return (Vec4) (((Vec3) v).cross((Vec3) *this));
+}
+
+
 Vec4 Vec4::rotate(const Quat& q) const{
-	
 	auto as_vec3 = Vec3(x,y,z);
 	auto rotated = as_vec3.rotate(q);
 	
-
 	return Vec4(rotated.x,rotated.y,rotated.z);
 }
--- src/Math/Vec4/Vec4.h
+++ src/Math/Vec4/Vec4.h
@@ -9,49 +9,87 @@ class Vec3;
 class Quat;
 #endif
 
-/* *
+/**
  * \class Vec4
- * \brief 4-component vector (x,y,z,w)
+ * \brief 4 component vector (x,y,z,w)
  * \author Joseph Cutler
  * \date
  */
 
 class Vec4{
 public:
-	float x; /**<x-component of vec4*/
-	float y; /**<y-component of vec4*/
-	float z; /**<z-component of vec4*/
-	float w; /**<w-component of vec4*/
+	float x; ///<X component of Vec4
+	float y; ///<Y component of Vec4
+	float z; ///<Z component of Vec4
+	float w; ///<W component of Vec4
 
-	Vec4();
+	///Default Constructor
+	/**
+	 * Default constructor for Vec4. Initializes all fields to 0 but W component, which starts as 1.
+	 */
+	Vec4(); 
+
+	///4 Component constructor
 	Vec4(float,float,float,float);
+
+	///3 Component constructor
+	/**
+	 * This constructor initializes W to 1.
+	 */
 	Vec4(float,float,float);
 
+	///Unit vector in +x direction
 	static const Vec4 I;
+	///Unit vector in +y direction
 	static const Vec4 J;
+	///Unit vector in +z direction
 	static const Vec4 K;
+	///Unit vector in +w direction
+	/**
+	 * Zero vector in R3
+	 */
 	static const Vec4 L;
 
+	///Magnitude (or size) of Vec4.
 	float mag() const;
 
 
+	///Sum of this and v
 	Vec4 operator+(const Vec4& v) const;
+	///Difference of this and v
 	Vec4 operator-(const Vec4& v) const;
+	///Scalar multiply this by f
 	Vec4 operator*(float f) const;
+	///Scalar divide this by f
 	Vec4 operator/(float f) const;
 
+	///Cast Vec4 to Vec3
+	/**
+	 * @warning Casting from Vec4 to Vec3 loses w component info
+	 */
 	operator Vec3() const;
 
+	///Returns a normalized version of this
 	Vec4 normalized() const;
-	void normalized();
 	static void normalize(Vec4& v);
 
+	///Dot product (scalar product) of v and this
 	float dot(const Vec4& v) const;
+
+	///Dot product (scalar product) of a and b
 	static float dot(const Vec4& a, const Vec4& b);
 
+	///Cross product (Vector product) of v and this
+	/**
+	 * @warning The cross product does not commute. AxB != BxA
+	 */
 	Vec4 cross(const Vec4& v) const;
-	void crossInPlace(const Vec4& v) const;
 
+
+	///Returns a rotated version of this by q.
+	/**
+	 * @warning Q must be a Unit Quaternion for the desired result
+	 */
 	Vec4 rotate(const Quat& q) const;
 
 };
