2e613a8a0b69c9969fec52ed31aa0e47355cbd96
Joseph Cutler <joseph_cutler@watkinson.org>
2016-04-12 10:20:42 -0400

    intersection works, draws circles.

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <cstdio>
 #include <vector>
 #include <limits>
+#include <cassert>
 #include "Math/Vec2/Vec2.h"
 #include "Math/Vec3/Vec3.h"
 #include "Math/Vec4/Vec4.h"
@@ -10,7 +11,6 @@
 #include "Math/Mat4/Mat4.h"
 #include "Math/Ray/Ray.h"
 #include "Math/Tree/Tree.h"
-
 #include "Graphics/Graphics.h"
 #include "OpenGL/OpenGL.h"
 #include <SDL2/SDL.h>
@@ -19,31 +19,41 @@
 //https://github.com/ssloy/tinyrenderer/wiki/Lesson-1:-Bresenham%E2%80%99s-Line-Drawing-Algorithm
 
 
+
 typedef struct Sphere{
 	Vec3 pos;
 	float r;
 } Sphere;
 
-/**
- * @note: x0<=x1
- */
-bool quad_solve(const float a, const float b, const float c, float &x0, float &x1){
-	auto discr = SQ(b) - 4*a*c;
+bool wierd_quad_solve(const float a, const float b, const float c, float &x0, float &x1){
+	auto discr = SQ(b) - a*c;
+//	printf("Discr: %f\n",discr);
 	if(discr < 0) return false;
-	x0 = (-b + sqrt(discr))/(2*a);
-	x1 = (-b - sqrt(discr))/(2*a);
+	x0 = (-b + sqrt(discr));
+	x1 = (-b - sqrt(discr));
 	if(x1 < x0) std::swap(x1,x0);
 	return true;
 }
 
 //https://en.wikipedia.org/wiki/Line%E2%80%93sphere_intersection
 bool sphere_ray_intersect_dist(const Sphere &sphere, const Ray &ray, float &d0, float &d1){
-	auto a = SQ(ray.dir.dot(ray.dir));
-	auto b = 2*(ray.dir.dot(ray.orig - sphere.pos));
-	auto c = SQ((ray.orig - sphere.pos).dot(ray.orig - sphere.pos)) - SQ(sphere.r);
+	auto o = ray.orig;
+	auto l = ray.dir;
+	auto x = sphere.pos;
+	auto r = sphere.r;
+
+	//Must be unit vector
+	assert(FLEQUAL(l.mag(),1));
+
+	auto a = l.dot(l);
+	//since l is unit length
+	assert(FLEQUAL(a,1));
+
+	auto b = (l.dot(o-x));
+	auto c = (o-x).squared() - SQ(r);
 	d0 = std::numeric_limits<float>::infinity();
 	d1 = std::numeric_limits<float>::infinity();
-	auto hit = quad_solve(a,b,c,d0,d1);
+	auto hit = wierd_quad_solve(a,b,c,d0,d1);
 	return hit;
 }
 
@@ -55,42 +65,64 @@ int main(int argc, char** argv){
 	Graphics g = Graphics(1920/2,1080/2,"Window");
 	g.Clear();
 
-	Sphere s0;
-	s0.pos = Vec3(0,0,100);
-	s0.r = 40.0;
-
 	Vec3 camera_pos = Vec3(0,0,-100);
 	Vec3 screen_pos = Vec3(0,0,0);
-	Vec3 screen_top_left = screen_pos + Vec3(-g.width/2,g.height/2,0);
+	Vec3 screen_top_left = screen_pos + Vec3(-static_cast<int>(g.width)/2,g.height/2,0);
 
-	float a0 = 0;
-	float a1 = 0;
-	quad_solve(2,-1,-6,a0,a1);
-	printf("Solved quadratic. Roots: %.2f, %.2f\n",a0,a1);
+	std::vector<Sphere> scene;
 
+	Sphere s0;
+	s0.pos = Vec3(0,0,30);
+	s0.r = 120;
+	scene.push_back(s0);
+
+	Sphere s1;
+	s1.pos = Vec3(50,50,130);
+	s1.r = 30;
+	scene.push_back(s1);
+
+	//might be the wrong way around;
+	//Z buffering
+	std::sort(scene.begin(),scene.end(),[](const Sphere &lhs, const Sphere &rhs){
+		return lhs.pos.z < rhs.pos.z;
+	});
+
+
+	P_VEC3(camera_pos);
+	P_VEC3(screen_pos);
+	P_VEC3(screen_top_left);
+
+	//For each pixel
 	for(int y = 0; y < g.height; ++y){
-		g.Clear();
 		for(int x = 0; x < g.width; ++x){
-			Ray cast_ray;
-			cast_ray.orig = camera_pos;
+			//for each object
+			for(const auto &object : scene){
+				
+				//construct a ray from the camera through the scrreen
+				Ray cast_ray;
+				cast_ray.orig = camera_pos;
 
-			auto px_vec = screen_top_left - camera_pos;
-			px_vec.x += x;
-			px_vec.y -= y;
-			cast_ray.dir = px_vec;
+				auto px_vec = camera_pos * SGN(camera_pos.z) + screen_top_left;
+				px_vec.x += x;
+				px_vec.y -= y;
 
-			float d0;
-			float d1;
+				cast_ray.dir = px_vec.normalized();
 
-			auto hit = sphere_ray_intersect_dist(s0,cast_ray,d0,d1);
-			float dist = std::min(d0,d1);
-			(void) dist;
 
-			if(hit){
-				g.PutPixel(x,y,Color::Red);
+				float d0;
+				float d1;
+
+				auto hit = sphere_ray_intersect_dist(object,cast_ray,d0,d1);
+				float dist = std::min(d0,d1);
+
+
+				Color px_color = Color::Black;
+
+				if(hit){
+					g.PutPixel(x,y,px_color);
+				}
 			}
 		}
-		g.Update();
 	}
 
 	g.Update();
@@ -109,5 +141,4 @@ int main(int argc, char** argv){
 
 	SDL_Quit();
 }
-
 
