df6fd01920bfc895516882197190b5380c2666c5
Joseph Cutler <joseph_cutler@watkinson.org>
2016-04-12 10:20:52 -0400

    adds some helpful macros

---
+++ src/macros.h
@@ -0,0 +1,13 @@
+#define VNAME(x) 					#x
+#define SGN(val) 					((0 < val) - (val < 0))
+#define SQ(x) 						(x*x)
+#define CLAMP(x, low, high)  		(((x) > (high)) ? (high) : (((x) < (low)) ? (low) : (x)))
+#define INEQ(l,type1,x,type2,h) 	(l type1 x && x type2 h)
+#define DEG(x)						(x*M_PI/180)
+#define RAD(x)						(x/M_PI*180)
+#define FLEQUAL(a,b) 				(std::abs(a - b) < 0.001)
+
+#define P_VEC3(v)	do{ \
+	printf("%s: %.3f,%.3f,%.3f\n",VNAME(v),v.x,v.y,v.z); \
+} while(0);
+
