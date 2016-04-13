06d675a71c74901bce1e0eaa5df94f295621d7d9
Joseph Cutler <joseph_cutler@watkinson.org>
2016-04-13 16:12:20 -0400

    srot of shading

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <limits>
 #include <cassert>
+#include <memory>
 #include "Math/Vec2/Vec2.h"
 #include "Math/Vec3/Vec3.h"
 #include "Math/Vec4/Vec4.h"
@@ -12,6 +13,8 @@
 #include "Math/Ray/Ray.h"
 #include "Math/Tree/Tree.h"
 #include "Graphics/Graphics.h"
+#include "Math/Sphere/Sphere.h"
+#include "Math/Object/Object.h"
 #include "OpenGL/OpenGL.h"
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_opengl.h>
@@ -19,44 +22,13 @@
 //https://github.com/ssloy/tinyrenderer/wiki/Lesson-1:-Bresenham%E2%80%99s-Line-Drawing-Algorithm
 
 
-
-typedef struct Sphere{
+typedef struct Light{
 	Vec3 pos;
-	float r;
-} Sphere;
+	float intensity;
+} Light;
 
-bool wierd_quad_solve(const float a, const float b, const float c, float &x0, float &x1){
-	auto discr = SQ(b) - a*c;
-//	printf("Discr: %f\n",discr);
-	if(discr < 0) return false;
-	x0 = (-b + sqrt(discr));
-	x1 = (-b - sqrt(discr));
-	if(x1 < x0) std::swap(x1,x0);
-	return true;
-}
 
 //https://en.wikipedia.org/wiki/Line%E2%80%93sphere_intersection
-bool sphere_ray_intersect_dist(const Sphere &sphere, const Ray &ray, float &d0, float &d1){
-	auto o = ray.orig;
-	auto l = ray.dir;
-	auto x = sphere.pos;
-	auto r = sphere.r;
-
-	//Must be unit vector
-	assert(FLEQUAL(l.mag(),1));
-
-	auto a = l.dot(l);
-	//since l is unit length
-	assert(FLEQUAL(a,1));
-
-	auto b = (l.dot(o-x));
-	auto c = (o-x).squared() - SQ(r);
-	d0 = std::numeric_limits<float>::infinity();
-	d1 = std::numeric_limits<float>::infinity();
-	auto hit = wierd_quad_solve(a,b,c,d0,d1);
-	return hit;
-}
-
 int main(int argc, char** argv){
 	(void) argc;
 	(void) argv;
@@ -72,25 +44,26 @@ int main(int argc, char** argv){
 	std::vector<Sphere> scene;
 
 	Sphere s0;
-	s0.pos = Vec3(0,0,30);
+	s0.pos = Vec3(-15,100,120);
 	s0.r = 120;
+	s0.c = Color(127,0,0);
 	scene.push_back(s0);
 
 	Sphere s1;
-	s1.pos = Vec3(50,50,130);
-	s1.r = 30;
+	s1.pos = Vec3(70,50,30);
+	s1.r = 15;
+	s1.c = Color::Blue;
 	scene.push_back(s1);
 
 	//might be the wrong way around;
 	//Z buffering
 	std::sort(scene.begin(),scene.end(),[](const Sphere &lhs, const Sphere &rhs){
-		return lhs.pos.z < rhs.pos.z;
+		return lhs.pos.z > rhs.pos.z;
 	});
 
-
-	P_VEC3(camera_pos);
-	P_VEC3(screen_pos);
-	P_VEC3(screen_top_left);
+	Light l0;
+	l0.pos = Vec3(20,80,10);
+	l0.intensity = 2000000;
 
 	//For each pixel
 	for(int y = 0; y < g.height; ++y){
@@ -102,24 +75,30 @@ int main(int argc, char** argv){
 				Ray cast_ray;
 				cast_ray.orig = camera_pos;
 
+				//get a vector from the camera through the pixel
 				auto px_vec = camera_pos * SGN(camera_pos.z) + screen_top_left;
 				px_vec.x += x;
 				px_vec.y -= y;
 
+				//Set and normalize the ray direction
 				cast_ray.dir = px_vec.normalized();
 
-
-				float d0;
-				float d1;
-
-				auto hit = sphere_ray_intersect_dist(object,cast_ray,d0,d1);
-				float dist = std::min(d0,d1);
-
-
-				Color px_color = Color::Black;
+				float dist; //unfortunately, we have to use the ugly fill-out-param pattern
+				auto hit = object.IntersectDist(cast_ray,dist);
 
 				if(hit){
-					g.PutPixel(x,y,px_color);
+					//Iterate over lights
+					auto intersection_point = cast_ray.orig + cast_ray.dir * dist;
+					auto normal = (intersection_point - object.pos).normalized();
+					float intensity_factor = std::abs(l0.intensity/SQ(dist));
+					USE(intensity_factor);
+					USE(normal);
+
+					Color ambient = object.c;
+					ambient.r = CLAMP(ambient.r + intensity_factor,0,255);
+
+
+					g.PutPixel(x,y,ambient);
 				}
 			}
 		}
