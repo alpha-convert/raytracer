a4c8f02ebde4542517bd4dee1f293779f37ff706
Joseph Cutler <joseph_cutler@watkinson.org>
2016-04-15 08:54:27 -0400

    added USE and INRANGE macros

--- src/macros.h
+++ src/macros.h
@@ -6,6 +6,8 @@
 #define DEG(x)						(x*M_PI/180)
 #define RAD(x)						(x/M_PI*180)
 #define FLEQUAL(a,b) 				(std::abs(a - b) < 0.001)
+#define USE(x)						((void) x)
+#define INRANGE(x,low,high)			INEQ(low,<=,x,<=,high)
 
 #define P_VEC3(v)	do{ \
 	printf("%s: %.3f,%.3f,%.3f\n",VNAME(v),v.x,v.y,v.z); \
