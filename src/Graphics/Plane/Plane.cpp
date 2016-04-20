946738ab2523fc4b9df55eb9e923fc900b5b758e
Joseph Cutler <joseph_cutler@watkinson.org>
2016-04-20 10:50:04 -0400

    plane inherits from object

--- src/Graphics/Plane/Plane.cpp
+++ src/Graphics/Plane/Plane.cpp
@@ -1,8 +1,12 @@
 #include "Plane.h"
+//https://www.cs.princeton.edu/courses/archive/fall00/cs426/lectures/raycast/sld017.htm
+//I love how I can learn from schools I can't get into
 bool Plane::IntersectDist(const Ray &ray, float &dist) const{
-	auto plane = *this;
-	//TODO: this obviously doesn't work
-	return false;
+	bool hit = false;
+	Vec3 P0 = ray.orig;
+	Vec3 V = ray.dir;
+
+	return hit;
 }
 
 Vec3 Plane::NormalAt(const Vec3 & p) const{
@@ -14,10 +18,20 @@ Plane::Plane(){
 	auto i = -std::numeric_limits<float>::infinity();
 	p = Vec3(i,i,i);
 	normal = Vec3(0,0,-1); //must be unit length or something will go horribly wrong
+
+	ks = 0.05;
+	kd = 0.8;
+	ka = 0.1;
+	alpha = 2;
 }
 
 Plane::Plane(Vec3 &p, Vec3& normal) : p(p){
 	assert(UNITLENGTH(normal));
 	this->normal = normal;
+
+	ks = 0.05;
+	kd = 0.8;
+	ka = 0.1;
+	alpha = 2;
 } 
 
--- src/Graphics/Plane/Plane.h
+++ src/Graphics/Plane/Plane.h
@@ -15,14 +15,13 @@
 #include "Vec3/Vec3.h"
 #include "macros.h"
 
-class Plane {
+class Plane : public Object{
 public:
 	Vec3 p;
 	Vec3 normal;
 
 	bool IntersectDist(const Ray &ray, float &dist) const;
 	Vec3 NormalAt(const Vec3 &p) const;
-	Color surface_color;
 	const static ObjectType type = ObjectType::type_plane;
 
 	Plane();
