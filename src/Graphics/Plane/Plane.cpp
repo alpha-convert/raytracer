b95474ea6c9b56841edadb97d23ebb108aa267a1
Joseph Cutler <joseph_cutler@watkinson.org>
2016-04-21 08:55:08 -0400

    fix sign and div by zero error with plane

--- src/Graphics/Plane/Plane.cpp
+++ src/Graphics/Plane/Plane.cpp
@@ -3,8 +3,17 @@
 //I love how I can learn from schools I can't get into
 bool Plane::IntersectDist(const Ray &ray, float &dist) const{
 	bool hit = false;
-	Vec3 P0 = ray.orig;
-	Vec3 V = ray.dir;
+	Vec3 Pr = ray.orig;
+	Vec3 P0 = p;
+	Vec3 n = normal;
+	Vec3 ur = ray.dir;
+
+	float denom = n.dot(ur);
+	if(denom < 0){
+		hit = true;
+		float num = n.dot(Pr - P0);
+		dist = -num/denom;
+	}
 
 	return hit;
 }
@@ -19,9 +28,9 @@ Plane::Plane(){
 	p = Vec3(i,i,i);
 	normal = Vec3(0,0,-1); //must be unit length or something will go horribly wrong
 
-	ks = 0.05;
-	kd = 0.8;
-	ka = 0.1;
+	ks = 0.2;
+	kd = 0.5;
+	ka = 0.3;
 	alpha = 2;
 }
 
