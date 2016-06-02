921072b262829baf6fbe22e58979159afb34365a
Joseph Cutler <joseph_cutler@watkinson.org>
2016-06-02 18:15:56 -0400

    fixex vectors in Vertexes

--- src/Math/Vertex/Vertex.h
+++ src/Math/Vertex/Vertex.h
@@ -1,12 +1,12 @@
 #ifndef VERTEX_H_
 #define VERTEX_H_ 
 #include <vector>
-#include "Vec4/Vec4.h"
+#include "Vec3/Vec3.h"
 class Vertex{
 public:
 	std::vector<unsigned> adj;
-	Vec4 pos;
-	Vertex(Vec4 pos, std::vector<unsigned> adj) : adj(adj), pos(pos){}
+	Vec3 pos;
+	Vertex(Vec3 pos, std::vector<unsigned> adj) : adj(adj), pos(pos){}
 };
 
 
