6ecc7660f090a7b05085c958732030eecc845089
Joseph Cutler <joseph_cutler@watkinson.org>
2017-07-28 12:48:29 -0400

    fixed some indenting... more to come.

--- src/Graphics/Graphics.cpp
+++ src/Graphics/Graphics.cpp
@@ -20,216 +20,216 @@
 //General graphics flow:
 
 Graphics::Graphics(uint32_t width, uint32_t height, const char *name) {
-	if (!SDL_WasInit(SDL_INIT_VIDEO)) {
-		SDL_Init(SDL_INIT_VIDEO);
-	}
-	this->width = width;
-	this->height = height;
+  if (!SDL_WasInit(SDL_INIT_VIDEO)) {
+    SDL_Init(SDL_INIT_VIDEO);
+  }
+  this->width = width;
+  this->height = height;
 
-	this->window = SDL_CreateWindow(name,
-			SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height,
-			SDL_WINDOW_SHOWN);
-	if (!this->window) {
-		fprintf(stderr, "Error creating window, %s\n", SDL_GetError());
-	}
-	assert(window != nullptr);
+  this->window = SDL_CreateWindow(name,
+      SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height,
+      SDL_WINDOW_SHOWN);
+  if (!this->window) {
+    fprintf(stderr, "Error creating window, %s\n", SDL_GetError());
+  }
+  assert(window != nullptr);
 
-	this->renderer = SDL_CreateRenderer(this->window, -1,
-			SDL_RENDERER_ACCELERATED);
-	if (!this->renderer) {
-		fprintf(stderr,"Error creating renderer: %s", SDL_GetError());
-	}
-	assert(renderer != nullptr);
+  this->renderer = SDL_CreateRenderer(this->window, -1,
+      SDL_RENDERER_ACCELERATED);
+  if (!this->renderer) {
+    fprintf(stderr,"Error creating renderer: %s", SDL_GetError());
+  }
+  assert(renderer != nullptr);
 
-	SDL_SetRenderDrawBlendMode(renderer,SDL_BLENDMODE_BLEND);
+  SDL_SetRenderDrawBlendMode(renderer,SDL_BLENDMODE_BLEND);
 
 }
 
 void Graphics::Clear() {
-	SetColor(Color::White);
-	SDL_RenderClear(this->renderer);
+  SetColor(Color::White);
+  SDL_RenderClear(this->renderer);
 }
 
 void Graphics::Update() {
-	for(const auto &p : points_to_draw){
-		SetColor(p.c);
-		SDL_RenderDrawPoint(renderer,p.p.x,p.p.y);
-	}
-	for(const auto &r : rects_to_draw){
-		SetColor(r.c);
-		SDL_RenderDrawRect(renderer,&r.r);
-	}
-	for(const auto &r : filled_rects_to_draw){
-		SetColor(r.c);
-		SDL_RenderFillRect(renderer,&r.r);
-	}
-	for(const auto &l : lines_to_draw){
-		SetColor(l.c);
-		SDL_RenderDrawLine(renderer,l.x1,l.y1,l.x2,l.y2);
-	}
-	SDL_UpdateWindowSurface(this->window);
-	SDL_RenderPresent(this->renderer);
+  for(const auto &p : points_to_draw){
+    SetColor(p.c);
+    SDL_RenderDrawPoint(renderer,p.p.x,p.p.y);
+  }
+  for(const auto &r : rects_to_draw){
+    SetColor(r.c);
+    SDL_RenderDrawRect(renderer,&r.r);
+  }
+  for(const auto &r : filled_rects_to_draw){
+    SetColor(r.c);
+    SDL_RenderFillRect(renderer,&r.r);
+  }
+  for(const auto &l : lines_to_draw){
+    SetColor(l.c);
+    SDL_RenderDrawLine(renderer,l.x1,l.y1,l.x2,l.y2);
+  }
+  SDL_UpdateWindowSurface(this->window);
+  SDL_RenderPresent(this->renderer);
 }
 
 void Graphics::ProjectVec3(const Vec3 &v, const Color &c, int scalar){
-	this->SpaceLine(0, 0, v.dot(Vec3::I) * scalar, v.dot(Vec3::J) * scalar, c);
+  this->SpaceLine(0, 0, v.dot(Vec3::I) * scalar, v.dot(Vec3::J) * scalar, c);
 }
 
 void Graphics::LineFromVec(const Vec3 &v1, const Vec3 &v2, const Color &c){
-	this->SpaceLine(v1.x, v1.y, v2.x, v2.y, c);
+  this->SpaceLine(v1.x, v1.y, v2.x, v2.y, c);
 }
 
 void Graphics::SpaceLine(int32_t x1, int32_t y1, int32_t x2, int32_t y2,const Color &c){
-	Line(this->width / 2 + x1, this->height / 2 - y1, this->width / 2 + x2,
-			this->height / 2 - y2, c);
+  Line(this->width / 2 + x1, this->height / 2 - y1, this->width / 2 + x2,
+      this->height / 2 - y2, c);
 }
 
 void Graphics::Line(int32_t x1, int y1, int x2, int y2, const Color &c){
-	DeferredRenderLine r;
-	r.c = c;
-	r.x1 = x1;
-	r.y1 = y1;
-	r.x2 = x2;
-	r.y2 = y2;
-	lines_to_draw.push_back(r);
+  DeferredRenderLine r;
+  r.c = c;
+  r.x1 = x1;
+  r.y1 = y1;
+  r.x2 = x2;
+  r.y2 = y2;
+  lines_to_draw.push_back(r);
 }
 
 void Graphics::PutPixel(int x, int y, const Color &c) {
-	if (!(x > this->width && x < 0 && y > this->height && y < 0)) {
-		DeferredRenderPoint p;
-		p.c = c;
-		p.p.x = x;
-		p.p.y = y;
-		points_to_draw.push_back(p);
-	}
+  if (!(x > this->width && x < 0 && y > this->height && y < 0)) {
+    DeferredRenderPoint p;
+    p.c = c;
+    p.p.x = x;
+    p.p.y = y;
+    points_to_draw.push_back(p);
+  }
 }
 
 Graphics::~Graphics() {
-	SDL_DestroyWindow(window);
-	SDL_DestroyRenderer(this->renderer);
-	SDL_Quit();
+  SDL_DestroyWindow(window);
+  SDL_DestroyRenderer(this->renderer);
+  SDL_Quit();
 }
 
 void Graphics::SetColor(const Color &c) {
-	SDL_SetRenderDrawColor(this->renderer, 255 * c.r, 255 * c.g, 255*c.b, 255*c.a);
+  SDL_SetRenderDrawColor(this->renderer, 255 * c.r, 255 * c.g, 255*c.b, 255*c.a);
 
 }
 
 Color Graphics::SDLColorToColor(uint32_t n) const {
-	Color c = Color(0,0,0);
-	c.r = (n & 0xff000000) / 255.0f;
-	c.g = (n & 0xff0000) / 255.0f;
-	c.b = (n & 0xff00) / 255.0f;
-	return c;
+  Color c = Color(0,0,0);
+  c.r = (n & 0xff000000) / 255.0f;
+  c.g = (n & 0xff0000) / 255.0f;
+  c.b = (n & 0xff00) / 255.0f;
+  return c;
 }
 
 //https://github.com/ssloy/tinyrenderer/wiki/Lesson-2:-Triangle-rasterization-and-back-face-culling
 Object *Graphics::GetClosestObject(const std::vector<Object *> &objects, const Ray &cast_ray, float &dist) const{
-	Object *closest_object = NULL;
-	dist = std::numeric_limits<float>::infinity();
-	for(const auto &object : objects){
-		float obj_dist;
-		auto hit_this = object->IntersectDist(cast_ray,obj_dist);
-		if(obj_dist < dist && hit_this){
-			dist = obj_dist;
-			closest_object = object;
-		}
-	}
-	return closest_object;
+  Object *closest_object = NULL;
+  dist = std::numeric_limits<float>::infinity();
+  for(const auto &object : objects){
+    float obj_dist;
+    auto hit_this = object->IntersectDist(cast_ray,obj_dist);
+    if(obj_dist < dist && hit_this){
+      dist = obj_dist;
+      closest_object = object;
+    }
+  }
+  return closest_object;
 
 }
 
 Object *Graphics::GetClosestObject(const std::vector<Object *> &objects, const Ray &cast_ray) const{
-	float dist = std::numeric_limits<float>::infinity();
-	Object *closest_object = NULL;
-	for(const auto &object : objects){
-		float obj_dist;
-		auto hit_this = object->IntersectDist(cast_ray,obj_dist);
-		if(obj_dist < dist && hit_this){
-			dist = obj_dist;
-			closest_object = object;
-		}
-	}
-	return closest_object;
+  float dist = std::numeric_limits<float>::infinity();
+  Object *closest_object = NULL;
+  for(const auto &object : objects){
+    float obj_dist;
+    auto hit_this = object->IntersectDist(cast_ray,obj_dist);
+    if(obj_dist < dist && hit_this){
+      dist = obj_dist;
+      closest_object = object;
+    }
+  }
+  return closest_object;
 
 }
 
 
 Color Graphics::Trace(const std::vector<Object *> &scene, const std::vector<Light> &lights, const Ray &cast_ray, const Vec3 &camera_pos,unsigned recurse_times) const{
-	//general init stuff
-	float closest_dist = std::numeric_limits<float>::infinity();
-	auto hit = false;
-	Color final_color = Color::White;
+  //general init stuff
+  float closest_dist = std::numeric_limits<float>::infinity();
+  auto hit = false;
+  Color final_color = Color::White;
 
-	//base case
-	if(recurse_times >= 3){
-		return final_color;
-	}
+  //base case
+  if(recurse_times >= 3){
+    return final_color;
+  }
 
-	//Find the closest object
-	Object *closest_object = GetClosestObject(scene,cast_ray,closest_dist);
-	hit = closest_object ? true : false;
+  //Find the closest object
+  Object *closest_object = GetClosestObject(scene,cast_ray,closest_dist);
+  hit = closest_object ? true : false;
 
-	assert(IMPLIES(hit,closest_object));
-	assert(IMPLIES(closest_dist == std::numeric_limits<float>::infinity(),!hit));
+  assert(IMPLIES(hit,closest_object));
+  assert(IMPLIES(closest_dist == std::numeric_limits<float>::infinity(),!hit));
 
 
-	if(hit){
-		Vec3 intersection_point = cast_ray.Along(closest_dist);
-		Vec3 normal = closest_object->NormalAt(intersection_point);
+  if(hit){
+    Vec3 intersection_point = cast_ray.Along(closest_dist);
+    Vec3 normal = closest_object->NormalAt(intersection_point);
 
-		Color ambient = closest_object->ColorAt(intersection_point);
-		final_color = ambient * closest_object->ka;
-		//Iterate over lights
+    Color ambient = closest_object->ColorAt(intersection_point);
+    final_color = ambient * closest_object->ka;
+    //Iterate over lights
 
-		//create the bounce ray
-		Ray bounce_ray;
-		bounce_ray.orig = intersection_point;
-		bounce_ray.dir = cast_ray.dir - normal * 2*(cast_ray.dir.dot(normal));
+    //create the bounce ray
+    Ray bounce_ray;
+    bounce_ray.orig = intersection_point;
+    bounce_ray.dir = cast_ray.dir - normal * 2*(cast_ray.dir.dot(normal));
 
-		//go over each light
-		for(const auto &l : lights){
-			float light_intersect_dist = (intersection_point - l.pos).mag();
-			USE(light_intersect_dist);
+    //go over each light
+    for(const auto &l : lights){
+      float light_intersect_dist = (intersection_point - l.pos).mag();
+      USE(light_intersect_dist);
 
-			//make a ray from from the light to the sphere
-			Ray light_check_ray;
-			light_check_ray.orig = l.test_sphere.pos;
-			light_check_ray.dir = (l.test_sphere.pos - intersection_point).normalized();
+      //make a ray from from the light to the sphere
+      Ray light_check_ray;
+      light_check_ray.orig = l.test_sphere.pos;
+      light_check_ray.dir = (l.test_sphere.pos - intersection_point).normalized();
 
-			//get the distance from the closest object to the light
-			float light_check_dist;
-			Object *closest_to_light = GetClosestObject(scene,light_check_ray,light_check_dist);
+      //get the distance from the closest object to the light
+      float light_check_dist;
+      Object *closest_to_light = GetClosestObject(scene,light_check_ray,light_check_dist);
 
-			//get the point where it hits
-			auto light_check_point = light_check_ray.orig + light_check_ray.dir * light_check_dist;
-			//and check if we're talking about the same point here
-			if(light_check_point == intersection_point){
-				Vec3 Lm = (light_check_point-l.pos).normalized();
-				Vec3 N = normal.normalized();
-				Vec3 Rm = N*2*(Lm.dot(N)) - Lm;
-				Vec3 V = (camera_pos - intersection_point).normalized();
-				float alpha = closest_to_light->alpha;
+      //get the point where it hits
+      auto light_check_point = light_check_ray.orig + light_check_ray.dir * light_check_dist;
+      //and check if we're talking about the same point here
+      if(light_check_point == intersection_point){
+        Vec3 Lm = (light_check_point-l.pos).normalized();
+        Vec3 N = normal.normalized();
+        Vec3 Rm = N*2*(Lm.dot(N)) - Lm;
+        Vec3 V = (camera_pos - intersection_point).normalized();
+        float alpha = closest_to_light->alpha;
 
-				float ks = closest_to_light->ks;
-				Color is = l.is;
-				Color specular_term = is * ks * std::pow(std::max<float>(Rm.dot(V),0),alpha);
-				assert(0<=specular_term.r<=1);
-				assert(0<=specular_term.g<=1);
-				assert(0<=specular_term.b<=1);
+        float ks = closest_to_light->ks;
+        Color is = l.is;
+        Color specular_term = is * ks * std::pow(std::max<float>(Rm.dot(V),0),alpha);
+        assert(0<=specular_term.r<=1);
+        assert(0<=specular_term.g<=1);
+        assert(0<=specular_term.b<=1);
 
-				float kd = closest_to_light->kd;
-				Color id = l.id;
-				Color diffuse_term = id * kd * std::max<float>(Lm.dot(N),0);
-				assert(0<=diffuse_term.r<=1);
-				assert(0<=diffuse_term.g<=1);
-				assert(0<=diffuse_term.b<=1);
+        float kd = closest_to_light->kd;
+        Color id = l.id;
+        Color diffuse_term = id * kd * std::max<float>(Lm.dot(N),0);
+        assert(0<=diffuse_term.r<=1);
+        assert(0<=diffuse_term.g<=1);
+        assert(0<=diffuse_term.b<=1);
 
-				Color reflection_term = Trace(scene,lights,light_check_ray,camera_pos,recurse_times+1) * closest_to_light->ks;
+        Color reflection_term = Trace(scene,lights,light_check_ray,camera_pos,recurse_times+1) * closest_to_light->ks;
 
-				final_color = (final_color + diffuse_term + specular_term);
-			}
-		}
-	}
-	return final_color;
+        final_color = (final_color + diffuse_term + specular_term);
+      }
+    }
+  }
+  return final_color;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -37,102 +37,102 @@ using json = nlohmann::json;
 using string = std::string;
 
 void read_entire_json_file(const std::string &fname, json &contents){
-	std::ifstream ifs (fname,std::ifstream::in);
-	ifs >> contents;
+  std::ifstream ifs (fname,std::ifstream::in);
+  ifs >> contents;
 }
 
 int main(int argc, char** argv){
-	USE(argc);
-	USE(argv);
+  USE(argc);
+  USE(argv);
 
-	//Audio a;
-	Graphics g = Graphics(1920/2,1080/2,"Raytracer");
-	g.Clear();
+  //Audio a;
+  Graphics g = Graphics(1920/2,1080/2,"Raytracer");
+  g.Clear();
 
-	//Texture lenna = Texture("textures/lenna.png");
+  //Texture lenna = Texture("textures/lenna.png");
 
-	//this doesn't really work but it's a start
-	constexpr float scene_angle = -M_PI/2; //angle from origin to camera
-	constexpr float camera_dist = 1500;
-	constexpr float screen_dist = 300;
+  //this doesn't really work but it's a start
+  constexpr float scene_angle = -M_PI/2; //angle from origin to camera
+  constexpr float camera_dist = 1500;
+  constexpr float screen_dist = 300;
 
-	//Note: camera is currently always pointing to the origin
-	Vec3 camera_pos = Vec3(camera_dist * cos(scene_angle),0,camera_dist * sin(scene_angle));
-	Vec3 camera_dir = Vec3::K;
-	USE(camera_dir);
-	Vec3 screen_pos = Vec3(screen_dist * cos(scene_angle),0,screen_dist * sin(scene_angle));
-	Vec3 screen_normal = (camera_pos - screen_pos).normalized();
-	Vec3 screen_top_left = screen_pos + Vec3(-static_cast<float>(g.width)/2,static_cast<float>(g.height)/2,0);
-	USE(screen_normal);
+  //Note: camera is currently always pointing to the origin
+  Vec3 camera_pos = Vec3(camera_dist * cos(scene_angle),0,camera_dist * sin(scene_angle));
+  Vec3 camera_dir = Vec3::K;
+  USE(camera_dir);
+  Vec3 screen_pos = Vec3(screen_dist * cos(scene_angle),0,screen_dist * sin(scene_angle));
+  Vec3 screen_normal = (camera_pos - screen_pos).normalized();
+  Vec3 screen_top_left = screen_pos + Vec3(-static_cast<float>(g.width)/2,static_cast<float>(g.height)/2,0);
+  USE(screen_normal);
 
-    //Global lists and maps
-	std::vector<Object *> scene;
-	std::vector<Light> lights;
-    auto gtm = std::make_shared<Texture::texturemap>();
+  //Global lists and maps
+  std::vector<Object *> scene;
+  std::vector<Light> lights;
+  auto gtm = std::make_shared<Texture::texturemap>();
 
-	json json_scene;
-	read_entire_json_file("planescene.json",json_scene);
+  json json_scene;
+  read_entire_json_file("planescene.json",json_scene);
 
 
-	for(const auto &o : json_scene["objects"]){
-		auto type = o["geom_info"]["type"];
-		if(type == "type_sphere"){
-			scene.push_back(new Sphere(o,gtm));
-		} else if(type == "type_plane"){
-			scene.push_back(new Plane(o,gtm));
-		} else if(type == "type_triangle"){
-			scene.push_back(new Triangle(o,gtm));
-		}
-	}
+  for(const auto &o : json_scene["objects"]){
+    auto type = o["geom_info"]["type"];
+    if(type == "type_sphere"){
+      scene.push_back(new Sphere(o,gtm));
+    } else if(type == "type_plane"){
+      scene.push_back(new Plane(o,gtm));
+    } else if(type == "type_triangle"){
+      scene.push_back(new Triangle(o,gtm));
+    }
+  }
 
-	for(const auto &ljson : json_scene["lights"]){
-		Light nl = Light(ljson);
-		lights.push_back(nl);
-        //Each light has a corresponding sphere in world space
-        //Todo: Figure out why I put this here
-		//Sphere s = Sphere();
-		//s = nl.test_sphere;
-		//scene.push_back(&s);
-	}
+  for(const auto &ljson : json_scene["lights"]){
+    Light nl = Light(ljson);
+    lights.push_back(nl);
+    //Each light has a corresponding sphere in world space
+    //Todo: Figure out why I put this here
+    //Sphere s = Sphere();
+    //s = nl.test_sphere;
+    //scene.push_back(&s);
+  }
 
-	//For each pixel
-	for(int y = 0; y < g.height; ++y){
-		for(int x = 0; x < g.width; ++x){
+  //For each pixel
+  for(int y = 0; y < g.height; ++y){
+    for(int x = 0; x < g.width; ++x){
 #if SUBPIXEL
-			Ray px_ray0 = Ray::ThroughPixel(x-0.5,y,camera_pos,screen_top_left);
-			Ray px_ray1 = Ray::ThroughPixel(x,y-0.5,camera_pos,screen_top_left);
-			Ray px_ray2 = Ray::ThroughPixel(x-0.5,y-0.5,camera_pos,screen_top_left);
-			Ray px_ray3 = Ray::ThroughPixel(x,y,camera_pos,screen_top_left);
-			Color c0 = g.Trace(scene,lights,px_ray0,camera_pos,0);
-			Color c1 = g.Trace(scene,lights,px_ray1,camera_pos,0);
-			Color c2 = g.Trace(scene,lights,px_ray2,camera_pos,0);
-			Color c3 = g.Trace(scene,lights,px_ray3,camera_pos,0);
-			Color final_color = c0.UnclampedAdd(c1).UnclampedAdd(c2).UnclampedAdd(c3) * (0.25);
+      Ray px_ray0 = Ray::ThroughPixel(x-0.5,y,camera_pos,screen_top_left);
+      Ray px_ray1 = Ray::ThroughPixel(x,y-0.5,camera_pos,screen_top_left);
+      Ray px_ray2 = Ray::ThroughPixel(x-0.5,y-0.5,camera_pos,screen_top_left);
+      Ray px_ray3 = Ray::ThroughPixel(x,y,camera_pos,screen_top_left);
+      Color c0 = g.Trace(scene,lights,px_ray0,camera_pos,0);
+      Color c1 = g.Trace(scene,lights,px_ray1,camera_pos,0);
+      Color c2 = g.Trace(scene,lights,px_ray2,camera_pos,0);
+      Color c3 = g.Trace(scene,lights,px_ray3,camera_pos,0);
+      Color final_color = c0.UnclampedAdd(c1).UnclampedAdd(c2).UnclampedAdd(c3) * (0.25);
 #else
-			Ray px_ray = Ray::ThroughPixel(x,y,camera_pos,screen_top_left);
-			Color final_color = g.Trace(scene,lights,px_ray,camera_pos,0);
+      Ray px_ray = Ray::ThroughPixel(x,y,camera_pos,screen_top_left);
+      Color final_color = g.Trace(scene,lights,px_ray,camera_pos,0);
 #endif
-			g.PutPixel(x,y,final_color);
-		}
-	}
+      g.PutPixel(x,y,final_color);
+    }
+  }
 
-	g.Update();
+  g.Update();
 
-	SDL_Event e;
-	bool running =  true;
-	while(running){
-		if(SDL_PollEvent(&e)){
-			if(e.type == SDL_QUIT || e.window.event == SDL_WINDOWEVENT_CLOSE){
-				running = false;
-			}
-		}
+  SDL_Event e;
+  bool running =  true;
+  while(running){
+    if(SDL_PollEvent(&e)){
+      if(e.type == SDL_QUIT || e.window.event == SDL_WINDOWEVENT_CLOSE){
+        running = false;
+      }
+    }
 
-	}
+  }
 
-	for(auto &a:scene) delete a;
-	SDL_Quit();
+  for(auto &a:scene) delete a;
+  SDL_Quit();
 
-    printf("%lu\n",gtm->size());
-    //printf("%p\n",gtm->begin());
-    //assert(gtm->empty());
+  printf("%lu\n",gtm->size());
+  //printf("%p\n",gtm->begin());
+  //assert(gtm->empty());
 }
